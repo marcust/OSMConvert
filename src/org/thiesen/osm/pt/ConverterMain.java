58031167959bd074850869655c8e40aae0572e41
Marcus Thiesen <marcus@thiesen.org>
2009-06-17 21:52:40 +0200

    remove old parsing code

--- src/org/thiesen/osm/pt/ConverterMain.java
+++ src/org/thiesen/osm/pt/ConverterMain.java
@@ -26,9 +26,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -36,14 +33,7 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.tools.bzip2.CBZip2InputStream;
-import org.thiesen.hhpt.shared.model.station.StationType;
 import org.thiesen.hhpt.shared.model.station.Stations;
-import org.thiesen.hhpt.shared.model.tag.StationTypeTagKey;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
@@ -59,8 +49,6 @@ public class ConverterMain {
 
         final Stations stationsNd = downloadNiedersachsen();
 
-        
-        
         final FileWriter writer = new FileWriter( "stations.csv" );
         writer.write( Stations.union( stationsHH, stationsNd ).asFileString() );
         writer.close();
@@ -132,94 +120,7 @@ public class ConverterMain {
     }
 
 
-    private static void addStationIfValid( final Element element, final Stations stations ) {
-        if ( "node".equalsIgnoreCase( element.getTagName() ) ) {
-            final String id = element.getAttribute( "id" );
-            final String longitude = element.getAttribute( "lon" );
-            final String latitude = element.getAttribute( "lat" );
-            String name = null;
-            StationType type = null;
-            String operator = null;
-            final NodeList childs = element.getChildNodes();
-            for (int i=0; i< childs.getLength(); i++) {
-                final Node node = childs.item( i );
-                if (node instanceof Element) {
-                    final Element el = (Element) node;
-
-                    final XMLTag t = XMLTag.valueOf( el.getAttribute( "k" ), el.getAttribute( "v" ) );
-                    if ( isOpertor( t ) ) {
-                        operator = t.getValue();
-                    }
-                    if ( isName( t ) ) {
-                        name = t.getValue();
-                    }
-                    if ( isStationType( t ) ) {
-                        type = StationType.valueOf( t.getKey(), t.getValue() );
-                    }
-
-                }
-            }
-
-            if ( type != null ) {
-                //System.out.println("Added " + type + " with name " + name );
-                stations.add( id, latitude, longitude, type, name, operator );
-            }
-
-
-        }
-    }
-
-
-    private static boolean isStationType( final XMLTag t ) {
-        return t.keyAnyOf( StationTypeTagKey.toXMLKeys() );
-
-    }
-
-    private static boolean isName( final XMLTag t ) {
-        return t.keyIs( "name" );
-    }
-
-    private static boolean isOpertor( final XMLTag t ) {
-        return t.keyIs( "operator" );
-    }
-
-    private static Element getRoot( final Document doc ) {
-        // Get the XML root node by examining the children nodes
-        final NodeList list = doc.getChildNodes();
-        for (int i=0; i<list.getLength(); i++) {
-            final Node node = list.item( i );
-            if (node instanceof Element) {
-                return (Element) node;
-            }
-        }
-
-        return null;
-    }
-
-    public static Document parseXmlFile(final InputStream input ) {
-        try {
-            // Create a builder factory
-            final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            factory.setValidating(false);
-
-            // Create the builder and parse the file
-            final Document doc = factory.newDocumentBuilder().parse( input );
-            return doc;
-        } catch (final SAXException e) {
-            handleException( e );
-        } catch (final ParserConfigurationException e) {
-            handleException( e );
-        } catch (final IOException e) {
-            handleException( e );
-        }
-        return null;
-    }
-
-    private static void handleException( final Exception e ) {
-        e.printStackTrace();
-
-    }
-
+  
     
     
 
